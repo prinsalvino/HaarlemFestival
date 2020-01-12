cc77bb616af947779b5642052fed5559933f93c3
TikkyTim <37704108+TikkyTim@users.noreply.github.com>
2020-01-12 18:50:46 +0100

    add ticket test 0.3

--- historyTickettest.php
+++ historyTickettest.php
@@ -10,7 +10,7 @@ session_start();
     <header></header>
     <body>
         <form>        
-            What ID number are we lookin' for chief? <br><input type="text" name="id"><br>
+            What ID number are we lookin' for chief? <br><input type="text" name="getTicketId"><br>
             <input type="Add ticket (id 1) to shoppingcart" name="action" value="Get item number id 1 from the database (as a test)"><br><br>
 		</form>
 	</body>
@@ -23,18 +23,17 @@ require(DB.php);
 
 
 //This uses the id you put in. It's just a test, as this ID can be either present to buttons you generate or the ID can be set by buttons you generate.
-$input = $_GET["id"];
+$getTicketId = $_GET["getTicketId"];
 
 //use the id to find the right one in the database.
 //1st, UserService extends DB but we can just access this method via an obj of class UserService instead of DB, better naming stuff, also I ofc belive you'll do all this in a proper method later
-//next where does your $ticketId come from while you get you id in $input :)
 //next $result array is already fetched in the method getTicketById($ticketId), so no need to do it again in the while loop, just get the array variable using a foreach loop, i mean i did it that way, you can see that in homepage.php
 //hope it wasn;t annoying, just trynna help :)
 $dal = NEW DB();
-$result = $dal->getTicketById($ticketId);
+$ticket = $dal->getTicketById($getTicketId);
 
 //For now, since I don't know how to make models, I made this to display the correct one. Plz forgive me.
-while($row = mysqli_fetch_array($result))
+foreach($ticket as $ticket)
 	{
 		ECHO "ID: " .$row['ticket_id'].'<br>';
         ECHO "Event: " .$row['event'].'<br>';
