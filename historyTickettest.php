e95d85cb7ea312c756179156b1e72998c9ae2faf
M.Bhat <42966101+muskan610@users.noreply.github.com>
2020-01-12 18:25:43 +0100

    suggestions

--- historyTickettest.php
+++ historyTickettest.php
@@ -26,6 +26,10 @@ require(DB.php);
 $input = $_GET["id"];
 
 //use the id to find the right one in the database.
+//1st, UserService extends DB but we can just access this method via an obj of class UserService instead of DB, better naming stuff, also I ofc belive you'll do all this in a proper method later
+//next where does your $ticketId come from while you get you id in $input :)
+//next $result array is already fetched in the method getTicketById($ticketId), so no need to do it again in the while loop, just get the array variable using a foreach loop, i mean i did it that way, you can see that in homepage.php
+//hope it wasn;t annoying, just trynna help :)
 $dal = NEW DB();
 $result = $dal->getTicketById($ticketId);
 
