e1aa22c91e6de7cceff34e0cd509984aa194356c
M.Bhat <42966101+muskan610@users.noreply.github.com>
2020-01-16 21:39:35 +0100

    header in progress

--- JazzHomepg.php
+++ JazzHomepg.php
@@ -1,4 +1,4 @@
-?php
+<?php
 
 declare(strict_types=1);
 session_start();
@@ -11,6 +11,30 @@ session_start();
 <script type="text/javascript" src="js/jazzScript.js" ></script>  
 </head>
 <body>
- 
+<header>
+<div class="headerRow">
+  <div class="headerColumn">
+  <img src="../img/BetterLogo.png" title="Haarlem festival" style="width:8vw; Height:8vw; margin-left:1vw">
+  </div>
+  <div class="headerColumn">
+  <img src="../img/titlelogo.png" title="Haarlem festival" style="width:25.25vw; Height:8vw">
+  </div>
+  <div class="headerColumn" >
+      
+    <div class="rowLoginCart">
+        <div class="columnLoginCart" style="background-color: red; text-align: center; margin-top:1vw">
+            <p> Log in <br> Or <br> Sign up  </p>
+        </div>
+        <div class="columnLoginCart" style="background-color: green; margin-top:1vw">
+            <img class=cartImg src="../img/shopping-cart.png" align="middle" title="cart/basket" style="width:5vw; Height:5vw">
+        </div>
+    </div>
+ </div>
+</div>
+    
+
+</header>
+
+
 </body>
 </html>
