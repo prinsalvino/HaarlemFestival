ee72f0b282e880c0a8b0bb0f73d9615234a886bc
FrancesMikaela <frances.mikalea03@gmail.com>
2020-01-17 14:36:59 +0100

    Created Dance Homepage file
    Css for dance homepage

---
+++ DanceHomepg.php
@@ -0,0 +1,13 @@
+<html class = "dance">
+<head>
+<meta charset="utf-8"/>
+<link rel="stylesheet" type="text/css" href="css/stylesheet.css">
+</head>
+<body>
+<?php include "header.php";?>
+
+
+<?php include "footer.php";?> 
+
+</body>
+</html>
