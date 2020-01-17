0cb256478cbf1ef6a75d4067d4661e9f0b602c21
M.Bhat <42966101+muskan610@users.noreply.github.com>
2020-01-17 12:38:41 +0100

    inlcude H en F

--- JazzHomepg.php
+++ JazzHomepg.php
@@ -13,64 +13,10 @@ session_start();
 <script type="text/javascript" src="js/jazzScript.js" ></script>  
 </head>
 <body>
-<header>
-<div class="headerRow">
-  <div class="headerColumn">
-    <img src="../img/BetterLogo.png" title="Haarlem festival" style="width:8vw; Height:8vw; margin-left:1vw">
-  </div>
-  <div class="headerColumn">
-    <img src="../img/titlelogo.png" title="Haarlem festival" style="width:25.25vw; Height:8vw">
-  </div>
-  <div class="headerColumn" >      
-    <div class="rowLoginCart">
-        <div class="columnLoginCart" style=" text-align: center; margin-top:1vw">
-                <b><a class="cartlink" href=""> Log in </a>
-                <br> Or <br> 
-                <a class="cartlink" href=""> Sign up  </a></b>
-        </div>
-        <div class="columnLoginCart" style=" margin-top:1vw">
-            <p> 
-                <img class=cartImg src="../img/shopping-cart.png" align="top" title="cart/basket" style="width:5vw; Height:5vw">
-                <br><br><br><br>
-                <b><a class="translink" href=""> NL/EN </a></b> 
-            </p>
-        </div>
-    </div>
-  </div>
-</div>  
-
-<nav class="navigbar" >
-	<ul> 	
-        <li><b>   <a class="navilink" href=""> Home  </a>   </b></li> 
-        <li><b>   <a class="navilink" href=""> Jazz  </a>   </b></li> 	
-        <li><b>   <a class="navilink" href=""> Dance </a>   </b></li> 
-        <li><b>   <a class="navilink" href=""> Food  </a>   </b></li> 	
-        <li><b>   <a class="navilink" href=""> Historic Food </a>   </b></li> 
-        <li><b>   <a class="navilink" href=""> Contact Us </a>   </b></li> 
-     </ul>
- </nav> 
- <br>
-</header>
+<?php include "header.php";?> 
 
 iets..
 
-<footer>
-  <div class="clearfix">
-    <!-- Add font awesome icons -->
-    <a href="#" class="fa fa-facebook icon"></a>
-    <a href="#" class="fa fa-instagram icon "></a>
-    <a href="#" class="fa fa-twitter icon"></a>
-    Follow us on: 
-  </div>
-  <!-- <br>
-  <br>
-  <br> -->
-  <div class = "labelfooter">
-    <b>
-    HAARLEM FESTIVAL 2020 <BR>
-    &copy; FINAL DRAFT
-    </b>
-</div>
-</footer>
+<?php include "footer.php";?> 
 </body>
 </html>
