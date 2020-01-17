9a42718d5b8f462cdf81f04610b4a4812be16b20
M.Bhat <42966101+muskan610@users.noreply.github.com>
2020-01-17 21:03:01 +0100

    dance is okay

--- DanceTicketpg.php
+++ DanceTicketpg.php
@@ -8,6 +8,9 @@ session_start();
 <head>
 <meta charset="utf-8"/>
 <link rel="stylesheet" type="text/css" href="css/stylesheet.css">
+<!-- Add icon library -->
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+<script type="text/javascript" src="js/jazzScript.js" ></script>  
 </head>
 <body>
 <?php include "header.php";?>
