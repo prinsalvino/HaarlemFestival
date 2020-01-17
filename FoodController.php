7324ec04102f69a205fd31bcf7acdfdb8522583e
prinsalvino <42965896+prinsalvino@users.noreply.github.com>
2020-01-17 12:47:36 +0100

    Food Stuff

---
+++ FoodController.php
@@ -0,0 +1,34 @@
+<?php
+
+require_once "FoodService.php";
+
+class FoodController
+{
+    private $FoodService = NULL; 
+
+	public function __construct()	
+	{
+		$this->FoodService = new FoodService();
+    }
+    
+    public function getAllRestaurant() 
+    { 
+        return $this->FoodService->getAllRestaurant(); 
+    }
+    
+	public function showAllRestaurant() 
+	{ 
+        $datas = $this->FoodService->getAllRestaurant(); 
+		foreach ($datas as $data) 
+		{
+			echo "Date : ".$data['date']."<br>"; 
+			echo "Time : ".$data['time']."<br>"; 
+			echo "Name : ".$data['location']."<br>"; 		
+			echo "Special : ".$data['special']."<br>"; 		
+			echo "Price : ".$data['price']."<br>"; 		
+		}
+	}
+}
+
+
+?>
---
+++ foodhome.php
@@ -0,0 +1,13 @@
+<!doctype html>
+<html>
+<?php
+	include ("header.php");
+	?>
+
+<body>
+</body>
+	
+	<?php
+	include ("footer.php");
+	?>
+</html>
