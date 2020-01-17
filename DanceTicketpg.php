828a900cb7be572b681736f3b44fefadc766e8ee
FrancesMikaela <frances.mikalea03@gmail.com>
2020-01-17 22:18:24 +0100

    Update DanceTicketpg.php

--- DanceTicketpg.php
+++ DanceTicketpg.php
@@ -8,7 +8,7 @@ session_start();
 <head>
 <meta charset="utf-8"/>
 <link rel="stylesheet" type="text/css" href="css/stylesheet.css">
-<link rel="stylesheet" type="text/css" href="css/jazzstylesheet.css">
+<link rel="stylesheet" type="text/css" href="css/dancestylesheet.css">
 <!-- Add icon library -->
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 <script type="text/javascript" src="js/jazzScript.js" ></script>  
