b86821f0e27310d171c9abf2b425a34b999876d5
prinsalvino <42965896+prinsalvino@users.noreply.github.com>
2020-01-17 14:06:11 +0100

    Testing

--- foodhome.php
+++ foodhome.php
@@ -1,5 +1,11 @@
 <!doctype html>
-<html>
+<html class = "Food">
+<head>
+<meta charset="utf-8"/>
+<link rel="stylesheet" type="text/css" href="css/stylesheet.css">
+<!-- Add icon library -->
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+</head>
 <?php
 	include ("header.php");
 	?>
@@ -7,7 +13,21 @@
 <body>
 </body>
 	
-	<?php
+<?php
 	include ("footer.php");
 	?>
 </html>
+
+<?php
+
+declare(strict_types=1);
+session_start();
+
+require(AutoLoaderIncl.php);
+require(DB.php);
+
+//Open a new DB to use for DB connections
+$dal = NEW DB();
+
+
+?>
