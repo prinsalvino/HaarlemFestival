6dc410954d504f8550a4dd9eb50665ef3e25e132
BrandonInholland <42965932+BrandonInholland@users.noreply.github.com>
2020-01-19 16:37:47 +0100

    designing dashboard and graph

---
+++ CMS/templates/include/cmsheader.php
@@ -0,0 +1,39 @@
+<header>  
+  <div class="headerRow">
+    <div class="headerColumn">
+      <img src="../img/BetterLogo.png" title="Haarlem festival" style="width:8vw; Height:8vw; margin-left:1vw">
+    </div>
+    <div class="headerColumn">
+      <img src="../img/titlelogo.png" title="Haarlem festival" style="width:25.25vw; Height:8vw">
+    </div>
+    <div class="headerColumn" >      
+      <div class="rowLoginCart">
+          <div class="columnLoginCart" style=" text-align: center; margin-top:1vw">
+                  <b><a class="cartlink" href="indexLogin.php"> Log in </a>
+                  <br> Or <br> 
+                  <a class="cartlink" href="signUpCustomer.php"> Sign up  </a></b>
+          </div>
+          <div class="columnLoginCart" style=" margin-top:1vw">
+              <p> 
+                  <img class=cartImg src="../img/shopping-cart.png" align="top" title="cart/basket" style="width:5vw; Height:5vw">
+                  <br><br><br><br>
+                  <b><a class="translink" href=""> NL/EN </a></b> 
+              </p>
+          </div>
+      </div>
+    </div>
+  </div>  
+
+  <nav class="navigbar" >
+    <ul> 	
+          <li><b>   <a class="navilink" href="index.php"> Home  </a>   </b></li> 
+          <li><b>   <a class="navilink" href="JazzHomepg.php"> Jazz  </a>   </b></li> 	
+          <li><b>   <a class="navilink" href="DanceHomepg.php"> Dance </a>   </b></li> 
+          <li><b>   <a class="navilink" href=""> Food  </a>   </b></li> 	
+          <li><b>   <a class="navilink" href="historyHomePage.php"> Historic Tour </a>   </b></li> 
+          <li><b>   <a class="navilink" href=""> Contact Us </a>   </b></li> 
+      </ul>
+  </nav> 
+  <br>
+</header>
+
