1acd5eb69c83a3d3dad42d85a88ea7cb0929e469
TikkyTim <37704108+TikkyTim@users.noreply.github.com>
2020-01-19 18:15:01 +0100

    shoppingcart 1.2

--- shoppingCartDB.php
+++ shoppingCartDB.php
@@ -9,18 +9,7 @@ class shoppingCartDB extends DB {
     { 
         $sql = "SELECT * FROM tickets"; 
         $result = $this->connect()->query($sql); 
-        $this->closeCon();
-
-        $numRows = $result->num_rows; 
-            if ($numRows > 0) 
-            {
-                while ($row = $result->fetch_assoc()) 
-                { 
-                    $data[] = $row; 
-                } 
-                return $data;  
-                
-            } 
+        return $result;        
     }
 
 ?>  
