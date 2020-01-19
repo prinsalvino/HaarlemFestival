299d551b4003043f72b3b24f7623fba73faf92d9
FrancesMikaela <frances.mikalea03@gmail.com>
2020-01-19 18:41:37 +0100

    Dance faqs

--- DanceFaqs.php
+++ DanceFaqs.php
@@ -33,12 +33,30 @@ session_start();
   </a>
 </div> 
 
-<div class = "danceDescriptionInfo">
-    <br> </br>Haarlem dance is a three day dance event where six of the best Dutch DJs who will fill the 
-    city with dance, trance, house, and techno music. <br> </br>
-    The event will be held on 27-29 of April 2020.
-    <br> </br>
+<div class = "danceFaqs">
+
+What is Haarlem Dance?
+Haarlem Dance is a special three-day programme where six of the best Dutch DJs will peform. 
+
+When is Haarlem Dance?
+It will be held on 27-29 of April 2020. 
+
+Where is Haarlem Dance?
+There will be different location for every artists. You could check the place here.
+
+What is the minimum age requirement for Haarlem Dance?
+The minimum age is 18. All visitors are obliged to carry valid prrof of identity, which must be displayed on request.
+
+Which artists will be appearing at Haarlem Dance?
+You could check the line up here.
+
+When will my favorite artist perform?
+You could check where and when is your favorite artist will perform on this page.
+
+Make sure to have lots of fun and don't forget to share your memories wth us on Facebook, Twitter, Instagram via #HaarlemDance. 
+
 </div>
+
 
 <?php include "footer.php";?> 
 
