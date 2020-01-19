05af3e62988dcfb1ed9a1d454c480b95e842125a
FrancesMikaela <frances.mikalea03@gmail.com>
2020-01-19 15:32:06 +0100

    Update DanceHomepg.php

--- DanceHomepg.php
+++ DanceHomepg.php
@@ -15,7 +15,6 @@ session_start();
 
 <div class = "danceDescriptionHome">Don't miss this exciting chance to eperience 
     a special three-day programme in Haarlem City!
-</br> </br> 
 </br> Catch Martin Garrix, Hardwell, Armin van Buuren, Tiesto, Nicky Romero, 
     and Afrojack in Haarlem Dance! Buy your tickets now! </br>
 </div>
