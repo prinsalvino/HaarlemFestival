f4baad5ee1c472f24cf892fecc82c5b0c15acda1
M.Bhat <42966101+muskan610@users.noreply.github.com>
2020-01-20 08:36:05 +0100

    Update jazzAllAccess.php

--- jazzAllAccess.php
+++ jazzAllAccess.php
@@ -13,35 +13,68 @@ session_start();
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 <script type="text/javascript" src="js/jazzScript.js" ></script>  
     <title>
-            Jazz D1 26 July 2020 || Haarlem Festival
+          All Day Access Tickets  || Haarlem Festival
     </title> 
 </head>
 <body>
 <?php include "header.php";?> 
 
-  <!-- <div class="cart-quantity">
-    Qty: 
-    <button onclick="increase_by_one('qty1');">+</button>
-    <input id="qty1" type="text" value="1" name="qty" />
-    <button onclick="decrease_by_one('qty1');" />-</button>
-  </div> -->
-
   <div class="rowTicketpg">
     <div class="columnTicketpg a" style="  margin-top:1vw; text-align: center; width: 30%;">
           <h1 class="inner" >
-          <b> Day 1 <br>
-                  July 26,2020
-                  <br><br>
-                  Thursday<br>
-                  The Patronaat</b>
-                  <br><br>
-                  <a href="#venueInfo" class="linkVenueInfo">(venue info)</a>
+
+          <?php
+          if($_GET['day'] == 26)
+            {
+          ?> 
+            <b> Day 1 <br>
+            July 26,2020
+            <br><br>
+            Thursday<br>
+             
+        <?php
+            }
+            else if($_GET['day'] == 27)
+            {
+        ?> 
+            <b> Day 2 <br>
+            July 27,2020
+            <br><br>
+            Friday<br>
+             
+        <?php
+            }
+            else if($_GET['day'] == 28)
+            {
+        ?> 
+        <b> Day 3 <br>
+            July 28,2020
+            <br><br>
+            Saturday<br>
+        <?php
+        }
+        else 
+        {
+        ?> 
+             <b> Day 1 <br>
+            July 26,2020
+            <br><br>
+            Thursday<br> 
+        <?php
+            }
+        ?> 
+        
+            The Patronaat</b>
+            <br><br>
+            <a href="#venueInfo" class="linkVenueInfo">(venue info)</a>
           </h1>
     </div>
 
 
     <div class= "columnTicketpg b" style="margin-top:1vw; width: 60%;">
 <!-- -------------------------------------------------------ROW 1---------------------------------------------------------------------------->
+      <p style="text-align: center; color: #fff"> <b> Book your <i>"All Day Access"</i> tickets here</b>
+    <br><br><br></p>
       <div class="rowTickets">
           <div class="columnTickets"  >
             <div class="row1">
@@ -92,11 +125,11 @@ session_start();
               </div>
             </div>
           </div>
-
-          
       </div>
 <!-- -------------------------------------------------------ROW 1----------------------------------------------------------------------------> 
-   
+  </div>
+</div>  
+
 <div id=venueInfo class=venueInfo>
   <h1><i><b> General Information about the venue </b> </i>
     <p>
