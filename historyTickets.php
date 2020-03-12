938a3328b12c22666a4c0efe62b2e647b36c5e75
TikkyTim <37704108+TikkyTim@users.noreply.github.com>
2020-03-12 21:57:11 +0100

    history tickets and background update

--- historyTickets.php
+++ historyTickets.php
@@ -1,4 +1,6 @@
 <?php
+include ("header.php");
+
 $connect = mysqli_connect("localhost","hfitteam1","3FxmuBcR","hfitteam1_db");
 
 if(isset($_POST["add_to_cart"]))
@@ -54,19 +56,17 @@ if(isset($_GET["action"]))
 <!DOCTYPE html>
 <html>
 	<head>
-		<title>Webslesson Demo | Simple PHP Mysql Shopping Cart</title>
 		<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.0/jquery.min.js"></script>
 		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" />
 		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
+        <link rel="stylesheet" href="css/stylesheet.css">
+		<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+		<link rel="stylesheet" href="css/historystyle.css">
 	</head>
 	<body>
 		<br />
 		<div class="container">
 			<br />
-			<br />
-			<br />
-			<h3 align="center">Tutorial - <a href="http://www.webslesson.info/2016/08/simple-php-mysql-shopping-cart.html" title="Simple PHP Mysql Shopping Cart">Simple PHP Mysql Shopping Cart</a></h3><br />
-			<br /><br />
 			<?php
                 //Over here 30478239048902384092384902384023890
 				$query = "SELECT * FROM tickets WHERE event='History'";
@@ -99,63 +99,5 @@ if(isset($_GET["action"]))
 					}
 				}
 			?>
-			<div style="clear:both"></div>
-			<br />
-			<h3>Order Details</h3>
-			<div class="table-responsive">
-				<table class="table table-bordered">
-					<tr>
-						<th width="40%">Item Name</th>
-						<th width="10%">Quantity</th>
-						<th width="20%">Price</th>
-						<th width="15%">Total</th>
-						<th width="5%">Action</th>
-					</tr>
-					<?php
-					if(!empty($_SESSION["shopping_cart"]))
-					{
-						$total = 0;
-						foreach($_SESSION["shopping_cart"] as $keys => $values)
-						{
-					?>
-					<tr>
-						<td><?php echo $values["ticket_id"]; ?></td>
-						<td><?php echo $values["amount"]; ?></td>
-						<td>$ <?php echo $values["price"]; ?></td>
-						<td>$ <?php echo number_format($values["amount"] * $values["price"], 2);?></td>
-						<td><a href="index.php?action=delete&id=<?php echo $values["ticket_id"]; ?>"><span class="text-danger">Remove</span></a></td>
-					</tr>
-					<?php
-							$total = $total + ($values["amount"] * $values["price"]);
-						}
-					?>
-					<tr>
-						<td colspan="3" align="right">Total</td>
-						<td align="right">$ <?php echo number_format($total, 2); ?></td>
-						<td></td>
-					</tr>
-					<?php
-					}
-					?>
-						
-				</table>
-			</div>
-		</div>
-	</div>
-	<br />
 	</body>
 </html>
-
-<?php
-//If you have use Older PHP Version, Please Uncomment this function for removing error 
-
-/*function array_column($array, $column_name)
-{
-	$output = array();
-	foreach($array as $keys => $values)
-	{
-		$output[] = $values[$column_name];
-	}
-	return $output;
-}*/
-?>
