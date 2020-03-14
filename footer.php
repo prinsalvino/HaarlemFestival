100bf5b201fea71d9185103a09aea6ef267b78d1
M.Bhat <42966101+muskan610@users.noreply.github.com>
2020-03-14 10:33:46 +0100

    icon lib in footer

--- footer.php
+++ footer.php
@@ -1,18 +1,37 @@
 <footer>
+<!-- Add icon library -->
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+
   <div class="clearfix">
+
     <!-- Add font awesome icons -->
+
     <a href="#" class="fa fa-facebook icon"></a>
+
     <a href="#" class="fa fa-instagram icon "></a>
+
     <a href="#" class="fa fa-twitter icon"></a>
+
     Follow us on: 
+
   </div>
+
   <!-- <br>
+
   <br>
+
   <br> -->
+
   <div class = "labelfooter">
+
     <b>
+
     HAARLEM FESTIVAL 2020 <BR>
+
     &copy; FINAL DRAFT
+
     </b>
+
   </div>
+
 </footer>
