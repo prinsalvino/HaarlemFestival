4cb81203c24e7028cf9f3962289616c226ce8232
M.Bhat <42966101+muskan610@users.noreply.github.com>
2020-03-16 10:43:11 +0100

    starting with shopping cart

--- shoppingCartService.php
+++ shoppingCartService.php
@@ -51,6 +51,24 @@ class ShopDB extends DB
 
     }
 
+    if (isset($_GET['customer_id'])){
+
+        $customer_id = $_GET['customer_id'];
+    
+        $ticket_id = $_GET['ticket_id'];
+    
+        $DB = new ShopDB();
+    
+        $delete = $common->deleteRecordById($connection,$customer_id, $ticket_id);
+    
+        if ($delete){
+    
+            echo '<script>alert("Record deleted successfully !")</script>';
+    
+            echo '<script>window.location.href="index.php";</script>';
+    
+        }
+
 }
 
 
