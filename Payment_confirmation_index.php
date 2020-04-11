fc3b02200d9664068b9501679f5c6d722596e8ec
M.Bhat <42966101+muskan610@users.noreply.github.com>
2020-04-11 16:00:59 +0200

    payment mollie fixed

--- Payment_confirmation_index.php
+++ Payment_confirmation_index.php
@@ -14,6 +14,7 @@ include "showErrors.php";
         <link rel="stylesheet" href="css/cartStylesheet.css">
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
         <script type="text/javascript" src="js/jazzScript.js" ></script> 
+        <meta http-equiv="Cache-control" content="no-cache" />
         <title>
             Order Confirmed || Haarlem Festival
         </title> 
--- pay.php
+++ pay.php
@@ -3,11 +3,6 @@ declare(strict_types=1);
 include "showErrors.php";
 session_start();
 
-
-if(!isset($_SESSION['email']))
- {
-     header("Location: index.php"); //if user is not logged in, redirect to user index pg
- }
 //page only accesible if user logged in
 
 
@@ -19,12 +14,16 @@ $mollie->setApiKey("test_kmu5pGPcpxGN72myAFhUm4SAWeprRn");
 $amount = $_POST['amount'];
 $description = $_POST['description'];
 
+// $amount = "12.50";
+// $description = "marketing";
+
 $payment = $mollie->payments->create([
   "amount" => [
     "currency" => "EUR",
     "value" => "$amount"
   ],
-//   "testmode" => true,
+
+  "method"      => \Mollie\Api\Types\PaymentMethod::IDEAL,
   "description" => "$description",
   "redirectUrl" => "http://hfitteam1.infhaarlem.nl/Payment_confirmation_index.php", 
   "webhookUrl"  => "http://hfitteam1.infhaarlem.nl/confirm.php",
