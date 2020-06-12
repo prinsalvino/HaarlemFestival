f1852cfad2664c5490c47708c2c1c16aa2a3cb33
TikkyTim <37704108+TikkyTim@users.noreply.github.com>
2020-06-12 16:00:41 +0200

    regular background for history tickets

--- historyTickets.php
+++ historyTickets.php
@@ -46,14 +46,8 @@ $connect = mysqli_connect("localhost","hfitteam1","3FxmuBcR","hfitteam1_db");
 				<?php $formNumber = $formNumber + 1; ?>
 
 			<div class="col-md-4">
-				
-				<?php 	$background = "#f1f1f1";
-						//if($row["special"] = "English"){$background = "lightblue";}
-						//else if($row["special"] = "Dutch"){$background = "orange";}
-						//else if($row["special" = "Chinese"]){$background = "lightred";}
-				?>
 
-				<div style="border:5px solid black; background-color:<?php echo $background ?>; border-radius:25px; padding:16px;" align="center">
+				<div style="border:5px solid black; background-color:#f1f1f1"; border-radius:25px; padding:16px;" align="center">
 
 						<p><?php echo $row["date"]; echo " || "; echo $row["time"]?></p>
 						<p><?php echo $row["special"]; ?></p>
