90021e22cdc8d41d0d1d15fd6dc3d998a66a8740
BrandonInholland <42965932+BrandonInholland@users.noreply.github.com>
2020-06-12 16:42:55 +0200

    old ocmmits

--- editEvents.php
+++ editEvents.php
@@ -64,29 +64,29 @@
         <input type="hidden" name="ticket_id" value="<?php echo $results['event']->ticket_id ?>"/>
 
         <ul class="editor">
-
-          <li>
+          
+        <li class="formedit">
             <label for="event">Event Type</label>
             <input type="text" name="event" id="event" placeholder="Type of Event" required maxlength= "20"<?php echo htmlspecialchars( $results['events']->event )?> />
           </li>
 
-          <li>
+          <li class="formedit">
             <label for="date">Date</label>
             <input type="date" name="date" id="date" placeholder="YYYY-MM-DD" required maxlength="10" value="<?php echo $results['events']->date ? date( "Y-m-d", $results['events']->date ) : "" ?>" />
           </li>
 
 
-          <li>
+          <li class="formedit">
             <label for="Time">Time</label>
             <input type="time" name= "time" id="time" placeholder="Time i.e. 18:00:00" required maxlength="8" value="<?php echo $results['events']->time ?> "/>
           </li>
 
-          <li>
+          <li class="formedit">
             <label for="location">Location</label>
             <input name="location" id="location" placeholder="Address or Restaurant Name" required maxlength="100"><?php echo htmlspecialchars( $results['events']->location )?>
           </li>
 
-          <li>
+          <li class="formedit">
             <label for="special">Special</label>
             <input name="special" id="special" placeholder="Artist/Type of Food/Language/Deal" required maxlength="100"><?php echo htmlspecialchars( $results['events']->special )?>
           </li>          
@@ -102,11 +102,13 @@
         <input type="submit" name="saveChanges" value="Save Changes" 
         style="position: relative;
         left: 317px;
-        top: -99px;">
+        top: -300px;
+        width: 160px;">
           <input type="submit" formnovalidate name="cancel" value="Cancel"
           style="position: relative;
             left: 317px;
-            top: -99px;" />
+            top: -300px;
+            width: 160px;" />
         
 
       </form>
